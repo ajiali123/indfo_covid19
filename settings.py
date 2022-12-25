8faac43cbd3b95c5e107eb7871c2e4667c128343
ajiali123 <86122352+ajiali123@users.noreply.github.com>
2022-12-25 21:15:59 +0800

    Add files via upload

--- settings.py
+++ settings.py
@@ -29,7 +29,7 @@ CSRF_TRUSTED_ORIGINS = [
     'https://indfocovid19-production-2929.up.railway.app'
 ]
 
-ALLOWED_HOSTS = ['indfocovid19-production-2929.up.railway.app']
+ALLOWED_HOSTS = ['https://indfocovid19-production-2929.up.railway.app']
 
 
 # Application definition
